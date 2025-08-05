9c5c78fb6b24220a4661efab7fc034fe4762467e
mallela224 <mallelas@roehampton.ac.uk>
2025-08-05 23:31:19 +0530

    Delete blockchain directory

--- blockchain/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Block
-
-class BlockSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Block
-        fields = ['id', 'index', 'timestamp', 'data', 'previous_hash', 'hash', 'nonce', 'proof_of_work_time'] 
--- blockchain/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import BlockListView
-
-urlpatterns = [
-    path('', BlockListView.as_view(), name='blockchain-list'),
-] 
